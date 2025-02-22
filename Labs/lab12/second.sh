17e562cce16030a6acb8a094aa5206ec4a5ad7cb
LaMeru <137993190+LaMeru@users.noreply.github.com>
2025-02-22 19:51:57 +0300

    Delete Labs/lab12 directory

--- Labs/lab12/second.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-i=1
-for a in "$@"; do
-	echo "$i: $a"
-		((i++))
-done
--- Labs/lab12/third.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "Введите путь к каталогу или папке"
-read ctlg
-change = $ctlg
-cd $change
-echo *
